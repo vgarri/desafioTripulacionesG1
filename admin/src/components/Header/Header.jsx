c6b5d2d465dd53efa618b1c439cda9a03bc8f276
JhonyBe77 <jjruiperezf@gmail.com>
2024-12-05 18:58:25 +0100

    addNav

--- admin/src/components/Header/Header.jsx
+++ admin/src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Nav from './Nav/Nav'
 
 const Header = () => {
-  return <div>Header</div>;
+  return <section id="header">
+    <Nav/>
+    <img src="https://felgtbi.org/wp-content/uploads/2021/10/cropped-logo_felgtbi.png" />
+  </section>;
 };
 
 export default Header;
--- admin/src/components/Main/Edicion/Edicion.jsx
+++ admin/src/components/Main/Edicion/Edicion.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
-const Edicion = () => {
-  return <div>Edicion</div>;
+const Nav = () => {
+  return (
+    <nav className="nav_generic">
+      <ul className="nav">
+        <li className="nav-link">
+          <Link className="btn-link" to="/">Gráficas</Link>
+        </li>
+        <li className="nav-link">
+          <Link className="btn-link" to="/edicion">Edición</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
-export default Edicion;
+export default Nav;
