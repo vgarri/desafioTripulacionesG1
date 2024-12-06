649b102ec1884c723bb1ab6414c2e69725df93f0
AnderlynDePaz <anderlyndepazhurtado@gmail.com>
2024-12-06 13:21:08 +0100

    modelo sequelize admin

--- config/db_pqSQL.js
+++ config/db_pqSQL.js
@@ -1,23 +1,55 @@
 require('dotenv').config();
-const pg = require('pg');
-const { Pool } = pg;
-//local
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE_AWS, 
+  process.env.DB_USER_AWS,   
+  String(process.env.DB_PASSWORD_AWS),
+  {
+    host: process.env.DB_HOST_AWS, 
+    port: process.env.DB_PORT,        
+    dialect: 'postgres',             
+    dialectOptions: {
+      ssl: false
+    },
+    logging: false, 
+  }
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión con la base de datos establecida correctamente.');
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error.message);
+  }
+})();
+
+module.exports = sequelize;
+
+
+
+
+// require('dotenv').config();
+// const pg = require('pg');
+// const { Pool } = pg;
+// //local
+// // let localPoolConfig = {
+// //     user: process.env.DB_USER,
+// //     password: process.env.DB_PASSWORD,
+// //     host: process.env.DB_HOST,
+// //     port: process.env.DB_PORT,
+// //     database: process.env.DB_DATABASE,
+// // };
 // let localPoolConfig = {
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER_RENDER,
+//     password: process.env.DB_PASSWORD_RENDER,
+//     host: process.env.DB_HOST_RENDER,
 //     port: process.env.DB_PORT,
-//     database: process.env.DB_DATABASE,
+//     database: process.env.DB_DATABASE_RENDER,
+//     ssl: true
 // };
-let localPoolConfig = {
-    user: process.env.DB_USER_RENDER,
-    password: process.env.DB_PASSWORD_RENDER,
-    host: process.env.DB_HOST_RENDER,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE_RENDER,
-    ssl: true
-};
 
-const pool = new Pool(localPoolConfig);
+// const pool = new Pool(localPoolConfig);
 
-module.exports = pool;
+// module.exports = pool;
