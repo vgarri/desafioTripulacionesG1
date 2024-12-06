67a4a2cd28d02dbd166b5a0860b8eeb6fbec4d4b
Victor Garritano <victor@getupgraded.com>
2024-12-06 17:29:41 +0100

    login, creacion de admin y bcrypt funcionando al 100% para admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.get('/test', adminController.testConnection)
 router.get('/', adminController.getAdmin);
+router.get('/email', adminController.getAdminByEmail)
 router.post('/login', adminController.login);
+router.post('/', adminController.createAdmin);
 router.get('/logout', adminController.logout);
 
 module.exports = router;
