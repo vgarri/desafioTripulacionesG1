0b90e77c3339b11e992e812b838ebaee90b36d9d
Victor Garritano <victor@getupgraded.com>
2024-12-07 19:14:22 +0100

    admin login working

--- admin/src/components/Main/Login/Login.jsx
+++ admin/src/components/Main/Login/Login.jsx
@@ -1,35 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
-const Login = ({ onLogin }) => {
+const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState("");
+  
 
-  const handleSubmit = (e) => {
+  //este useEffect testea la conexión con el Backend y la BBDD:
+  useEffect(() => {
+    const testConnection = async () => {
+      try {
+        const request = await axios({
+          method: 'get',
+          url: 'https://desafiotripulacionesg1.onrender.com/api/admin/test',
+          withCredentials: true
+        })
+        if (request) {
+          console.log()
+          setMessage("connected to server");
+          setTimeout(() => setMessage(""), 2000);
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    testConnection();
+  }, [])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Simulación de login
-    onLogin();
+    try{
+        const response = await axios({
+          method: 'post',
+          url: 'https://desafiotripulacionesg1.onrender.com/api/admin/login',
+          data: {email, password},
+          withCredentials: true
+        });
+        if (response.status === 200){
+          alert(`admin login was succesful`)
+          //actualizar el context y navigate al dashboard
+        }
+
+
+
+
+    }
+    catch (error){
+      alert(`wrong credentials`)
+    }
+
   };
 
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Iniciar sesión</button>
-      </form>
-    </div>
-  );
+
+  return <>
+    {/* {message !="" ?  :""} */}
+    <h1>Login</h1>
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  </>
+
 };
 
 export default Login;
