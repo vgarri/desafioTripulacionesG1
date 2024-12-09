4155c877bc3bc808624fb4cca56060e5c2cdc545
Victor Garritano <victor@getupgraded.com>
2024-12-09 12:12:27 +0100

    9diciembre

--- admin/src/components/Main/Login/Login.jsx
+++ admin/src/components/Main/Login/Login.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import Alert from '@mui/material/Alert';
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
 
   //este useEffect testea la conexión con el Backend y la BBDD:
@@ -15,8 +19,9 @@ const Login = ({ onLogin }) => {
           withCredentials: true
         })
         if (request) {
-          console.log()
+          
           setMessage("connected to server");
+          console.log(message)
           setTimeout(() => setMessage(""), 2000);
         };
       } catch (error) {
@@ -26,7 +31,30 @@ const Login = ({ onLogin }) => {
     testConnection();
   }, [])
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try{
+        const response = await axios({
+          method: 'post',
+          url: 'https://desafiotripulacionesg1.onrender.com/api/admin/login',
+          data: {email, password},
+          withCredentials: true
+        });
+        if (response.status === 200){
+          alert(`admin login was succesful`)
+          //actualizar el context y navigate al dashboard
+          
+        }
 
+
+
+
+    }
+    catch (error){
+      alert(`wrong credentials`)
+    }
+
+  };
 
 
 
@@ -44,8 +72,31 @@ const Login = ({ onLogin }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    try{
+      const response = await axios({
+        method: 'post',
+        url: 'https://desafiotripulacionesg1.onrender.com/api/admin/login',
+        data: {email, password},
+        withCredentials: true
+      });
+      if (response.status === 200){
+        onLogin();
+        alert(`admin login was succesful`)
+        
+        navigate('./graficas')
+        //actualizar el context y navigate al dashboard
+      }
 
-    onLogin();
+
+
+
+  }
+  catch (error){
+    alert(`wrong credentials`)
+  }
+
+ 
   };
 
   return (
@@ -72,6 +123,7 @@ const Login = ({ onLogin }) => {
           </button>
         </form>
       </div>
+
     </main>
   );
 
